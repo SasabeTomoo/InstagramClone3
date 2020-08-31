6574ba7409118dca20cadaade319d97dd3d6fcec
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-08-31 23:05:36 +0900

    新規投稿のbackボタンのリンク先の再修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users,    only: [:new, :create, :show]
-  root 'feeds#index'
   resources :feeds do
     collection do
       post :confirm
