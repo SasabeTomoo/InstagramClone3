1533ebc25a87ef530c87305bdfc4729d7ae8f107
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-09-01 14:43:46 +0900

    投稿のタイトル・内容にもバリデーションを加えた（presence:true）

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,5 +1,7 @@
 class Feed < ApplicationRecord
   validates :image, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
   belongs_to :user
   mount_uploader :image, ImageUploader
 end
