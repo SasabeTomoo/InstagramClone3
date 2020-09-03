5482ea635c524906a4332af02a7ac350673b90e9
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-09-03 17:08:45 +0900

    プロフィール画像：userとの紐付けをした

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,10 +4,14 @@ class ProfilesController < ApplicationController
   end
   def create
     @profile = Profile.new(profile_params)
-    @profile.save
-    redirect_to feeds_path, notice: 'プロフィール投稿に成功しました'
+    @profile.user_id = current_user.id
+    if @profile.save
+      redirect_to user_path(@profile.user_id), notice: 'プロフィール投稿に成功しました'
+    else
+      redirect_to user_path(@profile.user_id), notice: 'プロフィール投稿に失敗しました'
+    end
   end
 end
 def profile_params
-  params.require(:profile).permit(:image, :image_cache, :nickname, :content)
+  params.require(:profile).permit(:image, :image_cache, :nickname, :content, :user_id)
 end
