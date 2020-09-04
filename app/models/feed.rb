878189597240fd72bacfacfadc3833b4c98d5ec6
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-09-04 12:30:13 +0900

    Revert "first commit"
    This reverts commit ae241517db99ba724ba0d754f8a66f9780f2b1e1.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -3,11 +3,7 @@ class Feed < ApplicationRecord
   has_many :favorite_users, through: :favorites, source: :user
   belongs_to :user
   validates :image, presence: true
-<<<<<<< HEAD
   validates :title, presence: true
   validates :content, presence: true
-=======
-  belongs_to :user
->>>>>>> parent of 1533ebc... 投稿のタイトル・内容にもバリデーションを加えた（presence:true）
   mount_uploader :image, ImageUploader
 end
